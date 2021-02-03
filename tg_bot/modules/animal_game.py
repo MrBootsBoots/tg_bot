9ae63e6606154435931db000958f8b39fb45dfdd
MrBootsBoots <77720771+MrBootsBoots@users.noreply.github.com>
2021-02-03 15:04:59 +0530

    Update animal_game.py

--- tg_bot/modules/animal_game.py
+++ tg_bot/modules/animal_game.py
@@ -70,3 +70,42 @@ def animal4(bot: Bot, update: Update):
 @run_async
 def hint_animal2(bot: Bot, update: Update):
   update.message.reply_text(random.choice(HINT_ANIMAL2))
+
+)
+
+@run_async
+def hint_animal3(bot: Bot, update: Update):
+  update.message.reply_text(random.choice(HINT_ANIMAL3))
+
+)
+
+@run_async
+def hint_animal4(bot: Bot, update: Update):
+  update.message.reply_text(random.choice(HINT_ANIMAL4))
+
+)
+
+__help__ = """
+- /animal2 : animal name guessing game played between 2 players.
+- /hint(_)animal2 : animal game hint (type that cmd without bracket).
+- /animal3 : animal name guessing game played between 3 players.
+- /hint(_)animal3 : animal game hint (type that cmd without bracket).
+- /animal4 : animal guessing game played between 4 players.
+- /hint(_)animal4 : animal game hint (type that cmd without bracket).
+"""
+
+__mod_name__ = "Animal Game"
+
+ANIMAL2_HANDLER = DisableAbleCommandHandler("animal2", animal2)
+ANIMAL3_HANDLER = DisableAbleCommandHandler("animal3", animal3)
+ANIMAL4_HANDLER = DisableAbleCommandHandler("animal4", animal4)
+HINT_ANIMAL2_HANDLER = DisableAbleCommandHandler("hint_animal2", hint_animal2)
+HINT_ANIMAL3_HANDLER = DisableAbleCommandHandler("hint_animal3", hint_animal3)
+HINT_ANIMAL4_HANDLER = DisableAbleCommandHandler("hint_animal4", hint_animal4)
+
+dispatcher.add_handler(ANIMAL2_HANDLER)
+dispatcher.add_handler(ANIMAL3_HANDLER)
+dispatcher.add_handler(ANIMAL4_HANDLER)
+dispatcher.add_handler(HINT_ANIMAL2_HANDLER)
+dispatcher.add_handler(HINT_ANIMAL3_HANDLER)
+dispatcher.add_handler(HINT_ANIMAL4_HANDLER)
