d7bfb3fda980a0e3b3ed00f3d451c41a8d76752e
MrBootsBoots <77720771+MrBootsBoots@users.noreply.github.com>
2021-02-03 14:05:45 +0530

    Update feds.py

--- tg_bot/modules/feds.py
+++ tg_bot/modules/feds.py
@@ -1,1 +1,17 @@
+import html
+from io import BytesIO
+from typing import Optional, List
+import random
+import uuid
+import re
+import json
+import time
+from time import sleep
 
+from future.utlis import string_types
+from telegram.error import BadRequest, TelegramError, Unauthorized
+from telegram import PhraseMode, Update, Bot, Chat, User, MessageEntity, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import run_async, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram.utils.helpers import escape_markdown, mention_html, mention_markdown
+
+from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, WHITELIST_USERS, 
